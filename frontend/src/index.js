2d72d9c9a4ef187c8537f5e48ad918ff6698c9b3
Panagiotis Baltagiannis <46083188+PanosBal@users.noreply.github.com>
2020-10-08 12:37:23 +0300

    Create index.js

---
+++ frontend/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Employees from "./App.js"
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+
+ReactDOM.render(<Employees />, document.getElementById('root'));
