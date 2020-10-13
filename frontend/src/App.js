1f803ff7f6a4984ceed85515f4c90ae1d671ef9b
Panagiotis Baltagiannis <46083188+PanosBal@users.noreply.github.com>
2020-10-13 08:48:02 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,46 +3,68 @@ import axios from 'axios';
 import Create from './create';
 
 class Employees extends Component {
-  constructor() {
-    super();
-    this.state = {employees: []
+  constructor(props) {
+    super(props);
+    this.state = {employees: [], 
+      isEdit: false, index:'',indexName:'',indexDob:'',indexEmployee:''
       };
   }
+  
+   shouldComponentUpdate() {
+     return true;
+  }
 
-   getTable =  () => {
-    axios.get("http://localhost:5000/employees")
+   getTable = async ()  => {
+    await axios.get("http://localhost:5000/employees")
     .then(res => {
-      var employees = res.data;
-      //var x = JSON.parse(JSON.stringify(employees))
-      //console.log(x)
-      this.setState({ employees });
+      console.log(res.data);
+      var temp = res.data;      
+      this.setState({ employees:temp });
+      return(res.data)
     })
   }
-  
 
+
+  dobToNormal = (dob) => {
+    var date = new Date(dob);
+    
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var dt = date.getDate();
+    
+    if (dt < 10) {
+      dt = '0' + dt;
+    }
+    if (month < 10) {
+      month = '0' + month;
+    }
+    
+    date = year + '-' + month + '-' + dt;
+    return date;
+  }
 //DELETE----------------------------------------------------------------------
-  deleteAjax = (id) =>{
+  deleteAjax =  (id) =>{
     axios.get("http://localhost:5000/delete/" + id)
     .then(res => {
       console.log(res.data);
     })
   }
 
-  delete = (id) => { 
-    this.deleteAjax(id)
-      this.getTable();
+  delete = async (id) => {
+    this.cancel(); 
+    this.deleteAjax(id);
+    await this.getTable();
   }
 
 //CREATE----------------------------------------------------------------------
-createAjax = (info) =>{
-  axios.post("http://localhost:5000/create/",info)
+createAjax = async (info) =>{
+ await axios.post("http://localhost:5000/create/",info)
   .then(res => {
-    //console.log(res);
     console.log(res.data);
   })
 }
 
-Add = (xname,xdob) => {
+Add =  (xname,xdob) => {
   var temp = {name:xname,dob:xdob};
   this.createAjax(temp);
   this.getTable();
@@ -51,16 +73,44 @@ Add = (xname,xdob) => {
 //Update----------------------------------------------------------------------
 
 
+update = (employee) =>{
+  this.setState({isEdit:true})
+  this.setState({index:employee.id,indexName:employee.name,indexDob:employee.dob})
+  this.setState({indexEmployee:employee})
+  console.log(employee);
+}
 
+edit =  (xname,xdob) => {
+  //console.log(xname,xdob)
+  var temp = {name:xname,dob:xdob}
+  this.updateAjax(temp);
+    this.getTable();
+  this.setState({isEdit:false})
+}
 
+updateAjax = async (updateInfo) =>{
+   await axios.put("http://localhost:5000/update/" + this.state.index,updateInfo)
+  .then(res => {
+    console.log(res.data);
+  })
+}
 
+onSubmit = (event) => {
+  event.preventDefault();
+  this.edit(this.nameInput.value,this.dobInput.value);
+}
+
+cancel = () => {
+  this.setState({isEdit:false})
+}
 
 async componentDidMount(){
-  await  this.getTable() 
+  await this.getTable() 
 }
   
+
+
   render() {
-    
     return (
       <div className = "container">
         <table id="dtMaterialDesignExample" className="table" cellSpacing="0" width="100%">
@@ -78,9 +128,9 @@ async componentDidMount(){
             <tr key={employee.id}>
               <td>{employee.id}</td>
               <td>{employee.name}</td>
-              <td>{employee.dob}</td>
+              <td>{this.dobToNormal(employee.dob)}</td>
               <td>
-              <button  className="btn btn-success" onClick={() => this.Update(employee.id)}>Update</button>
+              <button  className="btn btn-success" onClick={() => this.update(employee)}>Update</button>
               </td>
               <td>
                 <button  className="btn btn-danger" onClick={() => this.delete(employee.id)}>Delete</button>
@@ -89,11 +139,28 @@ async componentDidMount(){
           ))}
         </tbody>
       </table>
-      <Create Add = {this.Add}/>   
+      {
+        this.state.isEdit ?(
+        <div>
+          <form onSubmit = {this.onSubmit}>
+          <h3>Update Employee</h3>
+          <input placeholder = "Name" ref={nameInput => this.nameInput = nameInput} defaultValue ={this.state.indexName}/>
+          <input placeholder = "Date of Birth" ref={dobInput => this.dobInput = dobInput} defaultValue ={this.dobToNormal(this.state.indexDob)}/>
+          <button className="btn-info">Edit</button>
+          <button  className="btn-warning" onClick={() => this.cancel()}>Cancel</button>
+          </form>
+          
+        </div>
+        ) : (
+          <Create Add = {this.Add}/>
+        )
+      }
+         
     </div> 
     
     );
   }
+  
 }
 
 
