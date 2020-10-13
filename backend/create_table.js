7af8fece1df029fd1dcdaade44f79eaf7024fdd2
Panagiotis Baltagiannis <46083188+PanosBal@users.noreply.github.com>
2020-10-13 08:47:53 +0300

    Update create_table.js

--- backend/create_table.js
+++ backend/create_table.js
@@ -7,10 +7,16 @@ var con = mysql.createConnection({
   database: "company"
 });
 
+
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  var sql = "CREATE TABLE IF NOT EXISTS employees (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(255), dob DATE, PRIMARY KEY(id))";
+  var sql = "SET GLOBAL time_zone='+03:00'";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Zone is ok");
+  });
+  var sql = "CREATE TABLE IF NOT EXISTS employees (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(255), dob DATETIME, PRIMARY KEY(id))";
   con.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Table created");
