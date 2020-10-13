16f7fc2dfb7e851d5a3df688a8fff8fa0fc6345c
Panagiotis Baltagiannis <46083188+PanosBal@users.noreply.github.com>
2020-10-13 08:47:55 +0300

    Update model.js

--- backend/model.js
+++ backend/model.js
@@ -5,30 +5,37 @@ var Employee = function(employee){
     this.dob = employee.dob;
 };
 
-Employee.createEmployee = function (newEmployee, result) {    
+Employee.createEmployee = function (newEmployee, result) { 
+
         sql.query("INSERT INTO employees set ?", newEmployee, function (err, res) {
                 if(err) {
                     console.log("error: ", err);
                     result(err, null);
                 }
                 else{
-                    console.log(res.insertId);
-                    result(null, res.insertId);
+                    console.log(res);
+                    result(null, res);
                 }
             });           
+
+    
 };
 
-Employee.getEmployee = function (result) {
-        sql.query("Select * from employees", function (err, res) {
+Employee.getEmployee =  function (result) {
+        sql.query("Select * from employees",async function (err, res) {
                 if(err) {
                     console.log("error: ", err);
                     result(null, err);
                 }
                 else{
-                  console.log("employees : ", res); 
-                 result(null, res);
+                console.log("employees : ", res); 
+                await result(null, res);
                 }
             });   
+            sql.query("SELECT @@session.time_zone,@@global.time_zone", function (err, result) {
+                if (err) throw err;
+                console.log(result);
+              });
 };
 
 Employee.updateById = function(id, employee, result){
